ffbd7ca21cb4d6c4edf5dd61fa6049b6ce527810
Guilherme Sol <guilherme_sol@hotmail.com>
2025-09-01 21:25:41 +0100

    Fix missing test plans from Tests

---
+++ PokePokemonApp/IntegrationTests/IntegrationTests.swift
@@ -0,0 +1,16 @@
+//
+//  IntegrationTests.swift
+//  IntegrationTests
+//
+//  Created by Guilherme Sol on 01/09/2025.
+//
+
+import Testing
+
+struct IntegrationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
